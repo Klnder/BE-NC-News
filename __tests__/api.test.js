d60108df611f41302b0ee97dd1afbe8d17b12175
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-10-02 11:05:27 +0100

    GET /api/topics done

---
+++ __tests__/api.test.js
@@ -0,0 +1,25 @@
+const app = require("../server");
+const request = require("supertest");
+
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("/api/topics", () => {
+  test("GET:200 return all topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const topics = body.topics;
+        expect(topics.length).toEqual(3);
+        topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
+        });
+      });
+  });
+});
---
+++ controllers/errors.controller.js
@@ -0,0 +1,5 @@
+function handlePsqlErrors(req, res, next) {}
+
+function handleCustomErrors(req, res, next) {}
+
+module.exports = { handleCustomErrors, handlePsqlErrors };
