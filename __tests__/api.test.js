2143a8a7106cca74b808b9fba9eb4019fc7d6ff1
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-10-02 14:05:16 +0100

    get /api Done

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,9 +1,11 @@
 const app = require("../server");
 const request = require("supertest");
+const fs = require("fs/promises");
 
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -20,6 +22,34 @@ describe("/api/topics", () => {
           expect(typeof topic.slug).toBe("string");
           expect(typeof topic.description).toBe("string");
         });
+      });
+  });
+});
+
+describe("/api", () => {
+  test("GET:200 return all api informations", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        const infos = body.endpoints;
+        expect(infos).toEqual(endpoints);
+        for (const info in infos) {
+          expect(infos[info]).toHaveProperty("description");
+          expect(infos[info]).toHaveProperty("queries");
+          expect(infos[info]).toHaveProperty("exampleResponse");
+        }
+      });
+  });
+});
+
+describe("/*", () => {
+  test("*:404 for any request type", () => {
+    return request(app)
+      .get("/afdf")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toEqual("path not valid");
       });
   });
 });
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const { handlePsqlErrors, handleCustomErrors } = require("./controllers/errors.controller");
 const { getTopics } = require("./controllers/topics.controller");
+const { getApiEndpoints } = require("./controllers/api.controller");
 const app = express();
+const port = 3000;
 
 // Middleware Connections
 app.use(express.json());
 
 // Routes
 app.get("/api/topics", getTopics);
+app.get("/api", getApiEndpoints);
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "path not valid" });
@@ -16,5 +19,10 @@ app.all("/*", (req, res, next) => {
 //Error handling middleware
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
+
+//listening
+// app.listen(port, () => {
+//   console.log(`API listening on port ${port}`);
+// });
 
 module.exports = app;
