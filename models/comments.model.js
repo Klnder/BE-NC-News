7acfe335e921a1b36764b2f43aef0d5fefa9609c
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-10-05 14:20:51 +0100

    patch comment done

--- models/comments.model.js
+++ models/comments.model.js
@@ -7,4 +7,25 @@ function removeComment(id) {
   });
 }
 
-module.exports = { removeComment };
+function updateComment(id, { inc_votes }) {
+  return selectCommentById(id)
+    .then(({ votes }) => {
+      const newVotes = votes + Number(inc_votes);
+      const query = `UPDATE comments SET votes=$1
+    WHERE comment_id=$2 RETURNING *;`;
+      return db.query(query, [newVotes, id]);
+    })
+    .then(({ rows }) => {
+      return rows[0];
+    });
+}
+
+function selectCommentById(id) {
+  const query = `SELECT * FROM comments WHERE comment_id=$1`;
+  return db.query(query, [id]).then(({ rows }) => {
+    if (!rows[0]) return Promise.reject({ status: 404, msg: "comment does not exist" });
+    return rows[0];
+  });
+}
+
+module.exports = { removeComment, updateComment };
